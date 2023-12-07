75658189e6b95520402efe6d5ece60361199d94f
Gomonenko Olga <lelik226@yandex.ru>
2023-12-07 16:06:10 +0900

    Добавлен роутинг, а также ссылки на разделы LXP

--- src/components/LeftPannel/Tabs/TabLeftMenu.tsx
+++ src/components/LeftPannel/Tabs/TabLeftMenu.tsx
@@ -1,30 +1,28 @@
-import TabElement from "./TabElement";
+// import TabElement from "./TabElement";
 import ChannelsIcon from "./TabsIcons/ChannelsIcon";
 import NewsIcon from "./TabsIcons/NewsIcon";
 import PopularIcon from "./TabsIcons/PopularIcon";
 import SavedIcon from "./TabsIcons/SavedIcon";
 import SubscribesIcon from "./TabsIcons/SubscribesIcon";
 import styles from "../../../style.module.css";
+import { Link, Route } from "react-router-dom";
+import TabRoute from "./TabRouteComponent";
+
 
 function TabLeftMenu() {
   return (
+    
     <ul className={styles.TabMenu}>
-      <TabElement name="Свежее">
-        <NewsIcon />
-      </TabElement>
-      <TabElement name="Популярное">
-        <PopularIcon />
-      </TabElement>
-      <TabElement name="Подписки">
-        <SubscribesIcon />
-      </TabElement>
-      <TabElement name="Каналы">
-        <ChannelsIcon />
-      </TabElement>
-      <TabElement name="Сохранённое">
-        <SavedIcon />
-      </TabElement>
+        <Link to="/">
+          <TabRoute name="Свежее"><NewsIcon /></TabRoute>
+        </Link> 
+        
+        <Link to="/_wt/eqvatoria_lxp_popular"> <TabRoute name="Популярное"><PopularIcon /></TabRoute></Link>
+        <Link to="/_wt/eqvatoria_lxp_subscribes"> <TabRoute name="Подписки"><SubscribesIcon /></TabRoute></Link>
+        <Link to="/_wt/eqvatoria_lxp_channels"> <TabRoute name="Каналы"><ChannelsIcon /></TabRoute></Link>
+        <Link to="/_wt/eqvatoria_lxp_saved"> <TabRoute name="Сохранённое"><SavedIcon /></TabRoute></Link>
     </ul>
+   
   );
 }
 
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,4 +1,3 @@
-import LeftZone from "./LeftPannel/LeftZone";
 import MainZone from "./MainZone/MainZone";
 import RightZone from "./RightZone/RightZone";
 import style from "../style.module.css";
@@ -6,7 +5,6 @@ import style from "../style.module.css";
 function MainPage() {
   return (
     <div className={style.MainPage}>
-      <LeftZone />
       <MainZone />
       <RightZone />
     </div>
