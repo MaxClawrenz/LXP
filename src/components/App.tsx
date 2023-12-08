38440ecbc65b3da9d16f6117e0bc0a50cee60a50
Gomonenko Olga <lelik226@yandex.ru>
2023-12-08 14:21:16 +0900

    Кнопка обложки канала

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,24 @@
-import { Routes, Router, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./Header/Header";
 import LeftZone from "./LeftPannel/LeftZone";
 import MainPage from "./MainPage";
+import MainPageChannels from "./MainPageChannels";
+import styles from "../style.module.css";
 
 
 function App() {
   return (
     <>
       <Header />
-      <LeftZone />
-      
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="*" element={<MainPage />} />
-      </Routes>
+      <div className={styles.mainContainer}>
+        <LeftZone />
+        
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="*" element={<MainPage />} />
+          <Route path="/_wt/eqvatoria_lxp_channels" element={<MainPageChannels />} />
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ src/components/MainPageChannels.tsx
@@ -0,0 +1,12 @@
+import style from "../style.module.css";
+import MainZoneChannels from "./MainZone/MainZoneChannels";
+
+function MainPageChannels() {
+  return (
+    <div className={style.MainPage}>
+      <MainZoneChannels />
+    </div>
+  );
+}
+
+export default MainPageChannels;
