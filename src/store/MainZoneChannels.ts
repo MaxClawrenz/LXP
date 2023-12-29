bab6e621365f25e18cb8003aa1918c546eaf04bc
Gomonenko Olga <lelik226@yandex.ru>
2023-12-29 17:02:23 +0900

    запрос в сторе

--- src/store/MainZoneChannels.ts
+++ src/store/MainZoneChannels.ts
@@ -12,7 +12,6 @@ class Channels {
     async getChannels() {
         try {
             const response = await axios.get('custom_web_template.html?object_code=my_channel_axios');
-            //console.log(response);
             runInAction(() => {
                     this.channelsArr = response.data;
                 }
