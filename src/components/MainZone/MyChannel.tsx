c8cb2f77504181b1f390897b506fc2948cc5bd65
Gomonenko Olga <lelik226@yandex.ru>
2024-01-12 21:02:53 +0900

    убрала тестовое отображение канала другого пользователя

--- src/components/MainZone/MyChannel.tsx
+++ src/components/MainZone/MyChannel.tsx
@@ -8,24 +8,7 @@ function MyChannel(props:IMyChannel) {
   
     return (
     <div className={style_channels.channel_block}>
-        {/* <MyChannelInfo 
-                key={props.channelId}
-                channelId={props.channelId}
-                channelName={props.channelName}
-                authorID={props.authorID}
-                channelDecript={props.channelDecript}
-                avatarChannelID={props.avatarChannelID}
-                coverChannelID={props.coverChannelID}
-                arrPostsInChannel={props.arrPostsInChannel}
-                arrSubscriptionsInChannel={props.arrSubscriptionsInChannel}
-                arrKnowlegesParts={props.arrKnowlegesParts}
-                arrTags={props.arrTags}
-                authorFullname={props.authorFullname}
-                arrComments={props.arrComments} 
-                arrCountSubscriptionsChannels={props.arrCountSubscriptionsChannels}
-                /> */}
-
-              <OtherChannel 
+        <MyChannelInfo 
                 key={props.channelId}
                 channelId={props.channelId}
                 channelName={props.channelName}
@@ -41,6 +24,23 @@ function MyChannel(props:IMyChannel) {
                 arrComments={props.arrComments} 
                 arrCountSubscriptionsChannels={props.arrCountSubscriptionsChannels}
                 />
+
+              {/* <OtherChannel 
+                key={props.channelId}
+                channelId={props.channelId}
+                channelName={props.channelName}
+                authorID={props.authorID}
+                channelDecript={props.channelDecript}
+                avatarChannelID={props.avatarChannelID}
+                coverChannelID={props.coverChannelID}
+                arrPostsInChannel={props.arrPostsInChannel}
+                arrSubscriptionsInChannel={props.arrSubscriptionsInChannel}
+                arrKnowlegesParts={props.arrKnowlegesParts}
+                arrTags={props.arrTags}
+                authorFullname={props.authorFullname}
+                arrComments={props.arrComments} 
+                arrCountSubscriptionsChannels={props.arrCountSubscriptionsChannels}
+                /> */}
     </div>
     )
   }
