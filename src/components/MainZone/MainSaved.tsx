1f79f284b0d27f8cb8dc9e91c02c94aba7012a66
Максим Туктаров <maksimtuktarov@MacBook-Air-Maksim.local>
2024-02-27 12:24:19 +0700

    temporary-tab-saved-posts

---
+++ src/components/MainZone/MainSaved.tsx
@@ -0,0 +1,62 @@
+import { observer } from "mobx-react-lite";
+import saved from "../../store/savedPosts";
+import { useEffect, useRef } from "react";
+import PostCard from "./PostCard";
+import styles from "../../style.module.css";
+
+function MainSaved() {
+  const bottomOfList = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const newsObserver = new IntersectionObserver(
+      (entries) => {
+        if (
+          entries[0].isIntersecting &&
+          !saved.isLoading &&
+          saved._target !== "empty"
+        )
+          saved.getSaved();
+      },
+      {
+        rootMargin: "0px",
+        threshold: 1.0,
+      }
+    );
+
+    if (bottomOfList.current) newsObserver.observe(bottomOfList.current);
+
+    return () => {
+      if (bottomOfList.current) newsObserver.unobserve(bottomOfList.current);
+    };
+  }, [bottomOfList.current]);
+
+  return (
+    <div className={styles.MainZone}>
+      {saved.savedArr.map((post) => (
+        <PostCard
+          id={post.id}
+          key={post.id}
+          channel_pict={post.channel_pict}
+          channel_name={post.channel_name}
+          channel_id={post.channel_id}
+          knowledge_name={post.knowledge_name}
+          time_posted={post.time_posted}
+          is_follow={post.is_follow}
+          post_name={post.post_name}
+          post_text={post.post_text}
+          likes_count={post.likes_count}
+          my_like={post.my_like}
+          comments_count={post.comments_count}
+          favourite_count={post.favourite_count}
+          my_favourite={post.my_favourite}
+          create_date={post.create_date}
+          is_my_blog={post.is_my_blog}
+          blog_id={post.blog_id}
+        />
+      ))}
+      {saved.isLoading && <div>Loading...</div>}
+      <div ref={bottomOfList} className={styles.news_observer}></div>
+    </div>
+  );
+}
+
+export default observer(MainSaved);
