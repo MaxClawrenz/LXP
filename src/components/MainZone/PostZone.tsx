625f1a299efa51cf29f33803616a79b4a75d8ea7
Максим Туктаров <maksimtuktarov@MacBook-Air-Maksim.local>
2024-04-02 18:13:26 +0700

    добавлен скелет в страницу поста

--- src/components/MainZone/PostZone.tsx
+++ src/components/MainZone/PostZone.tsx
@@ -8,6 +8,7 @@ import { observer } from "mobx-react-lite";
 import postBody from "../../store/postBody";
 import PostImages from "./PostImages";
 import PostFooter from "./PostFooter";
+import UniversalSkelet from "./UniversalSkelet";
 
 function PostZone() {
   const { hours, minutes } = useTime(postBody.post.time_posted);
@@ -34,36 +35,153 @@ function PostZone() {
               </div>
               <Link to={`/_wt/lxp_channels/${postBody.post.channel_id}`}>
                 <div className={style.channelBlock}>
-                  <img
-                    className={style.channelPict}
-                    src={postBody.post.channel_pict}
-                    alt="Картинка канала"
-                  />
+                  {!postBody.isLoading ? (
+                    <img
+                      className={style.channelPict}
+                      src={postBody.post.channel_pict}
+                      alt="Картинка канала"
+                    />
+                  ) : (
+                    <UniversalSkelet
+                      width={"20px"}
+                      height={"20px"}
+                      marginTop={""}
+                      marginBottom={""}
+                      marginLeft={""}
+                      marginRight={"11px"}
+                      paddingTop={""}
+                      paddingBottom={""}
+                      paddingLeft={""}
+                      paddingRight={""}
+                      borderRadius={"4px"}
+                    />
+                  )}
                   <div className={style.channelName}>
-                    {postBody.post.channel_name}
+                    {!postBody.isLoading ? (
+                      postBody.post.channel_name
+                    ) : (
+                      <UniversalSkelet
+                        width={"104px"}
+                        height={"17px"}
+                        marginTop={""}
+                        marginBottom={""}
+                        marginLeft={""}
+                        marginRight={""}
+                        paddingTop={""}
+                        paddingBottom={""}
+                        paddingLeft={""}
+                        paddingRight={""}
+                        borderRadius={"4px"}
+                      />
+                    )}
                   </div>
                 </div>
               </Link>
               <div className={style.cardKnowledge}>
-                {postBody.post.knowledge_name}
+                {!postBody.isLoading ? (
+                  postBody.post.knowledge_name
+                ) : (
+                  <UniversalSkelet
+                    width={"260px"}
+                    height={"17px"}
+                    marginTop={""}
+                    marginBottom={""}
+                    marginLeft={""}
+                    marginRight={"50px"}
+                    paddingTop={""}
+                    paddingBottom={""}
+                    paddingLeft={""}
+                    paddingRight={""}
+                    borderRadius={"4px"}
+                  />
+                )}
               </div>
               <div className={style.cardTime}>
-                {hours > 24
-                  ? postBody.post.create_date
-                  : hours < 24 && hours
-                  ? `${hours} ${hoursName}`
-                  : minutes
-                  ? `${minutes} ${minutesName}`
-                  : "1 минута"}
+                {!postBody.isLoading ? (
+                  hours > 24 ? (
+                    postBody.post.create_date
+                  ) : hours < 24 && hours ? (
+                    `${hours} ${hoursName}`
+                  ) : minutes ? (
+                    `${minutes} ${minutesName}`
+                  ) : (
+                    "1 минута"
+                  )
+                ) : (
+                  <UniversalSkelet
+                    width={"76px"}
+                    height={"17px"}
+                    marginTop={""}
+                    marginBottom={""}
+                    marginLeft={""}
+                    marginRight={""}
+                    paddingTop={""}
+                    paddingBottom={""}
+                    paddingLeft={""}
+                    paddingRight={""}
+                    borderRadius={"4px"}
+                  />
+                )}
               </div>
             </div>
           </div>
-          <div className={style.cardTitle}>{postBody.post.post_name}</div>
+          <div className={style.cardTitle}>
+            {!postBody.isLoading ? (
+              postBody.post.post_name
+            ) : (
+              <UniversalSkelet
+                width={"100%"}
+                height={"24px"}
+                marginTop={""}
+                marginBottom={"15px"}
+                marginLeft={""}
+                marginRight={""}
+                paddingTop={""}
+                paddingBottom={""}
+                paddingLeft={""}
+                paddingRight={""}
+                borderRadius={"4px"}
+              />
+            )}
+          </div>
         </div>
         <div className={style.postText}>
-          <pre className={style.bodyText}>{postBody.post.post_text}</pre>
+          <pre className={style.bodyText}>
+            {!postBody.isLoading ? (
+              postBody.post.post_text
+            ) : (
+              <>
+                <UniversalSkelet
+                  width={"100%"}
+                  height={"16px"}
+                  marginTop={"16px"}
+                  marginBottom={"2px"}
+                  marginLeft={""}
+                  marginRight={""}
+                  paddingTop={""}
+                  paddingBottom={""}
+                  paddingLeft={""}
+                  paddingRight={""}
+                  borderRadius={"4px"}
+                />
+                <UniversalSkelet
+                  width={"80%"}
+                  height={"16px"}
+                  marginTop={""}
+                  marginBottom={"16px"}
+                  marginLeft={""}
+                  marginRight={""}
+                  paddingTop={""}
+                  paddingBottom={"4px"}
+                  paddingLeft={""}
+                  paddingRight={""}
+                  borderRadius={"4px"}
+                />
+              </>
+            )}
+          </pre>
         </div>
-        {postBody.post.file_id && (
+        {!postBody.isLoading && postBody.post.file_id && (
           <PostImages fileIds={postBody.post.file_id} />
         )}
         <PostFooter
