daebda392989f758f3c78056458670b37d9632ae
Gomonenko Olga <lelik226@yandex.ru>
2024-05-22 23:53:36 +0900

    Добавление автора канала

--- src/store/EditFormChannel.ts
+++ src/store/EditFormChannel.ts
@@ -5,6 +5,7 @@ import { makeAutoObservable, runInAction } from "mobx";
 class EditFormChannel {
     result: number = 0;
     arrAuthors: any[] = [];
+    arrCollaborators: any[] = [];
     
     constructor() {
         makeAutoObservable(this, {
@@ -12,25 +13,35 @@ class EditFormChannel {
         });
     } 
 
-    async deleteAuthor(id_channel:number, id_author:number, arrAuthors: any) {
+    async deleteAuthor(id_channel: number, id_author: number) {
         try {
-            const response = await axios.get('/custom_web_template.html?object_code=delete_author_channel_lxp',
-            {params: {
+            console.log('Отправка запроса на сервер с данными:', { id_author, id_channel });
+    
+            const response = await axios.post('/custom_web_template.html?object_code=delete_author_channel_lxp', null, {
+                params: {
                 id_author: id_author,
                 id_channel: id_channel
-              }
-            }
-            );
-            
-            runInAction(() => {
-                    this.result = response.data;
-                    this.arrAuthors = this.arrAuthors.filter((author: { id: number; }) => author.id !== id_author);
                 }
-            )
+            });
+
+    
+            // Логирование ответа сервера для отладки
+            console.log('Ответ сервера:', response.data);
+    
+            // Проверка на успешный ответ
+            if (response.data === 1) {
+                runInAction(() => {
+                    this.arrAuthors = this.arrAuthors.filter((author: { id: number; }) => author.id !== id_author);
+                });
+            } else {
+                console.error('Ошибка удаления автора канала: Неверный ответ от сервера', response.data);
+            }
         } catch (error) {
-            console.error('Ошибка получения каналов пользователя: ', error);
+            console.error('Ошибка удаления автора канала: ', error);
         }
     }
+    
+    
 
     async saveChannelChanges(forma: any) {
         try {
@@ -49,6 +60,48 @@ class EditFormChannel {
             )
         } catch (error) {
             console.error('Ошибка получения каналов пользователя: ', error);
+        }
+    }
+
+    async searchAuthor(input_value: string) {
+        try {
+            const response = await axios.get('/custom_web_template.html?object_code=search_author_lxp', {
+                params: {
+                    input_user_value: input_value
+                }
+            });
+            
+            runInAction(() => {
+                this.result = response.data;
+                this.arrCollaborators = response.data;
+            });
+            return response.data; // Возвращаем данные для использования в компоненте
+        } catch (error) {
+            console.error('Ошибка поиска автора: ', error);
+            return []; // Возвращаем пустой массив в случае ошибки
+        }
+    }
+
+
+    async addNewAuthor(collId: number, collFullname: string, channelId: number) {
+        try {
+            const response = await axios.post('/custom_web_template.html?object_code=add_new_author_lxp', null, {
+                params: {
+                    id_author: collId,
+                    id_channel: channelId,
+                    coll_fullname: collFullname
+                }
+            });
+            
+            runInAction(() => {
+                this.result = response.data;
+                this.arrAuthors = response.data;
+            });
+            
+            return response.data;
+        } catch (error) {
+            console.error('Ошибка добавления автора: ', error);
+            return [];
         }
     }
 
